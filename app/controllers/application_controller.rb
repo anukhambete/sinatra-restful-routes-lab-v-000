44c3621717f440434108394243f2d1af82b0815f
Anu Khambete <anukhambete@gmail.com>
2018-08-06 22:01:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,8 +43,8 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes/:id' do #edit action
     @recipe = Recipe.find_by_id(params[:id])
-    @recipe.name = params[:name]
-    @recipe.ingredients = params[:ingredients]
+    @recipe.name = params[:name] unless params[:name].empty?
+    @recipe.ingredients = params[:ingredients] unless params[:ingredients].empty?
     @recipe.cook_time = params[:cook_time]
     @recipe.save
     redirect to "/recipes/#{@recipe.id}"
